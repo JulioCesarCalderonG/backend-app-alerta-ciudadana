f841c2b971b760b1b9b93d6e2c56615d3e49bc2d
Julio Cesar Calderon Galindo <julcescalgalty@gmail.com>
2022-11-17 15:59:33 -0500

    mostrando las alertas e imagenes

--- controllers/tipo-alertas.js
+++ controllers/tipo-alertas.js
@@ -1,91 +1,127 @@
 const { response, request } = require("express");
-const {TipoAlerta} = require("../models");
+const fs = require("fs");
+const path = require("path");
+const { subirArchivo } = require("../helpers");
 
+const { TipoAlerta } = require("../models");
 
-
-const getTipoAlertas = async(req=request, res=response) =>{
-    const {estado} =req.query;
-    const tipoalerta = await TipoAlerta.findAll({
-        where:{
-            estado
-        }
+const getTipoAlertas = async (req = request, res = response) => {
+  const { estado } = req.query;
+  const tipoalerta = await TipoAlerta.findAll({
+    where: {
+      estado,
+    },
+  });
+  try {
+    res.json({
+      ok: true,
+      msg: "Se muestran todas las alertas",
+      tipoalerta,
     });
-    try {
-        res.json({
-            ok:true,
-            msg:'Se muestran todas las alertas',
-            tipoalerta
-        })
-    } catch (error) {
-        res.status(400).json({
-            ok:false,
-            msg:`Error: ${error}`
-        })
+  } catch (error) {
+    res.status(400).json({
+      ok: false,
+      msg: `Error: ${error}`,
+    });
+  }
+};
+const getTipoAlerta = async (req = request, res = response) => {
+  try {
+    res.json({
+      ok: true,
+    });
+  } catch (error) {
+    res.status(400).json({
+      ok: false,
+      msg: `Error: ${error}`,
+    });
+  }
+};
+const mostrarImagenTipoAlerta = async (req = request, res = response) => {
+  try {
+    const { id } = req.params;
+    const tipoalerta = await TipoAlerta.findOne({
+      where: {
+        id,
+      },
+    });
+    if (!tipoalerta) {
+      const respuesta = path.join(__dirname, "../assets/", "no-image.png");
+      return res.sendFile(respuesta);
     }
-}
-const getTipoAlerta = async(req=request, res=response) =>{
-    try {
-        res.json({
-            ok:true
-        })
-    } catch (error) {
-        res.status(400).json({
-            ok:false,
-            msg:`Error: ${error}`
-        })
+    const pathImagen = path.join(
+      __dirname,
+      "../uploads/",
+      "tipos-alertas",
+      tipoalerta.img
+    );
+    if (fs.existsSync(pathImagen)) {
+      return res.sendFile(pathImagen);
+    } else {
+      const pathDefault = path.join(__dirname, "../assets/", "no-image.png");
+      return res.sendFile(pathDefault);
     }
-}
-const postTipoAlerta = async(req=request, res=response) =>{
-    try {
-        const {nombre, ...data} =req.body;
-        data.nombre = nombre.toUpperCase();
-        const tipoalerta = await TipoAlerta.create(data);
-        res.json({
-            ok:true,
-            msg:'Tipo de alerta creada con exito',
-            tipoalerta
-        })
-    } catch (error) {
-        res.status(400).json({
-            ok:false,
-            msg:`Error: ${error}`
-        })
-    }
-}
-const putTipoAlerta = async(req=request, res=response) =>{
-    try {
-        res.json({
-            ok:true
-        })
-    } catch (error) {
-        res.status(400).json({
-            ok:false,
-            msg:`Error: ${error}`
-        })
-    }
-}
-const deleteTipoAlerta = async(req=request, res=response) =>{
-    try {
-        res.json({
-            ok:true
-        })
-    } catch (error) {
-        res.status(400).json({
-            ok:false,
-            msg:`Error: ${error}`
-        })
-    }
-}
-
-
-
-
-
+  } catch (error) {
+    res.status(400).json({
+      ok: false,
+      msg: `Error: ${error}`,
+    });
+  }
+};
+const postTipoAlerta = async (req = request, res = response) => {
+  try {
+    const { nombre, ...data } = req.body;
+    const file = req.files;
+    const img = await subirArchivo(
+      file,
+      ["png", "jpg", "jpeg", "gif"],
+      "tipos-alertas"
+    );
+    data.nombre = nombre.toUpperCase();
+    data.img = img;
+    const tipoalerta = await TipoAlerta.create(data);
+    res.json({
+      ok: true,
+      msg: "Tipo de alerta creada con exito",
+      tipoalerta,
+    });
+  } catch (error) {
+    res.status(400).json({
+      ok: false,
+      msg: `Error: ${error}`,
+    });
+  }
+};
+const putTipoAlerta = async (req = request, res = response) => {
+  try {
+    res.json({
+      ok: true,
+    });
+  } catch (error) {
+    res.status(400).json({
+      ok: false,
+      msg: `Error: ${error}`,
+    });
+  }
+};
+const deleteTipoAlerta = async (req = request, res = response) => {
+  try {
+    res.json({
+      ok: true,
+    });
+  } catch (error) {
+    res.status(400).json({
+      ok: false,
+      msg: `Error: ${error}`,
+    });
+  }
+};
 
 module.exports = {
-    getTipoAlertas,
-    getTipoAlerta,
-    postTipoAlerta,
-    putTipoAlerta,
-    deleteTipoAlerta
-}
+  getTipoAlertas,
+  getTipoAlerta,
+  mostrarImagenTipoAlerta,
+  postTipoAlerta,
+  putTipoAlerta,
+  deleteTipoAlerta,
+};
--- routes/tipo-alertas.js
+++ routes/tipo-alertas.js
@@ -1,16 +1,18 @@
 const { Router } = require("express");
 const { check } = require("express-validator");
-const { getTipoAlertas, getTipoAlerta, postTipoAlerta, putTipoAlerta, deleteTipoAlerta } = require("../controllers/tipo-alertas");
+const { getTipoAlertas, getTipoAlerta, postTipoAlerta, putTipoAlerta, deleteTipoAlerta, mostrarImagenTipoAlerta } = require("../controllers/tipo-alertas");
 const { nombreTipoAlerta } = require("../helpers");
-const { validarCampos } = require("../middlewares");
+const { validarCampos, validarArchivoSubir } = require("../middlewares");
 
 
 const router = Router();
 
 router.get('/',getTipoAlertas);
 router.get('/:id',getTipoAlerta);
+router.get('/mostrar/imagen/:id',mostrarImagenTipoAlerta)
 router.post('/',[
     check('nombre').custom(nombreTipoAlerta),
+    validarArchivoSubir,
     validarCampos
 ],postTipoAlerta);
 router.put('/:id',putTipoAlerta);
