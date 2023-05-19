9aa1c92a6295f3b6282b7ccb5e2091fb84a4357a
Julio Cesar Calderon Galindo <julcescalgalty@gmail.com>
2023-05-19 11:04:34 -0500

    cambios

--- controllers/ciudadanos.js
+++ controllers/ciudadanos.js
@@ -1,5 +1,5 @@
 const { request, response } = require("express");
-const { Ciudadano } = require("../models");
+const { Ciudadano, DetalleCiudadano } = require("../models");
 const bcryptjs = require("bcryptjs");
 const { generarJWT } = require("../helpers");
 const jwt = require("jsonwebtoken");
@@ -7,7 +7,7 @@ const { Op } = require("sequelize");
 const getCiudadanos = async (req = request, res = response) => {
   try {
     const { estado, buscar } = req.query;
-    if (buscar==="") {
+    if (buscar === "") {
       const ciudadano = await Ciudadano.findAll({
         where: {
           estado,
@@ -19,30 +19,28 @@ const getCiudadanos = async (req = request, res = response) => {
         ciudadano,
       });
     }
-    const ciudadano = await Ciudadano.findAll(
-      {
-        where:{
-          estado,
-          [Op.or]:[
-            {
-              dni:{
-                [Op.startsWith]:`%${buscar}%`
-              }, 
+    const ciudadano = await Ciudadano.findAll({
+      where: {
+        estado,
+        [Op.or]: [
+          {
+            dni: {
+              [Op.startsWith]: `%${buscar}%`,
             },
-            {
-              nombre:{
-                [Op.startsWith]:`%${buscar}%`
-              }, 
+          },
+          {
+            nombre: {
+              [Op.startsWith]: `%${buscar}%`,
             },
-            {
-              apellido:{
-                [Op.startsWith]:`%${buscar}%`
-              }, 
-            }
-          ]        
-        }
-      }
-    );
+          },
+          {
+            apellido: {
+              [Op.startsWith]: `%${buscar}%`,
+            },
+          },
+        ],
+      },
+    });
     res.json({
       ok: true,
       msg: "Ciudadanos mostrados con exito",
@@ -77,7 +75,7 @@ const getCiudadano = async (req = request, res = response) => {
 };
 const postCiudadano = async (req = request, res = response) => {
   try {
-    const { password, dni, ...data } = req.body;
+    const { password, dni,celular, ...data } = req.body;
 
     // Verificamos si el ciudadano ya esta registrado
     const resp = await Ciudadano.findOne({
@@ -106,7 +104,8 @@ const postCiudadano = async (req = request, res = response) => {
     data.dni = dni;
     // Realizamos la subida del usuario al BD
     const ciudadano = await Ciudadano.create(data);
-
+    // Realizamos la subida a la tabla detalle ciudadano
+    const detalleCiudadano = await DetalleCiudadano.create({celular,id_ciudadano:ciudadano.id});
     // Generamos un token al nuevo usuario
     const token = await generarJWT(ciudadano.id);
 
@@ -138,19 +137,25 @@ const updateCiudadano = async (req = request, res = response) => {
 
 const deleteCiudadano = async (req = request, res = response) => {
   try {
-    const {id} = req.params;
-    const {estado} = req.query;
-    const ciudadano = await Ciudadano.update({
-      estado
-    },{
-      where:{
-        id
+    const { id } = req.params;
+    const { estado } = req.query;
+    const ciudadano = await Ciudadano.update(
+      {
+        estado,
+      },
+      {
+        where: {
+          id,
+        },
       }
-    })
+    );
     res.json({
       ok: true,
-      msg:(estado === '0') ? 'Se bloqueo el centro de atencion' :'Se desbloqueo el centro de atencion',
-      ciudadano
+      msg:
+        estado === "0"
+          ? "Se bloqueo el centro de atencion"
+          : "Se desbloqueo el centro de atencion",
+      ciudadano,
     });
   } catch (error) {
     res.status(400).json({
@@ -165,13 +170,13 @@ const actualizarPassword = async (req = request, res = response) => {
     const { token } = req.params;
     const { password } = req.body;
     const { id } = jwt.verify(token, process.env.SECRETORPRIVATEKEY);
-    
+
     // leer el ciudadano
 
     const ciudadano = await Ciudadano.findOne({
-      where:{
-        id
-      }
+      where: {
+        id,
+      },
     });
     if (!ciudadano) {
       return res.json({
@@ -193,14 +198,19 @@ const actualizarPassword = async (req = request, res = response) => {
     // Creamos un password Hasheado
     const salt = bcryptjs.genSaltSync();
     const hasPassword = bcryptjs.hashSync(password, salt);
-    const resp = await Ciudadano.update({password:hasPassword}, {where:{
-      id:ciudadano.id
-    }})
-    
+    const resp = await Ciudadano.update(
+      { password: hasPassword },
+      {
+        where: {
+          id: ciudadano.id,
+        },
+      }
+    );
+
     res.json({
       ok: true,
-      msg:'Password Actualizado',
-      resp
+      msg: "Password Actualizado",
+      resp,
     });
   } catch (error) {
     res.status(400).json({
