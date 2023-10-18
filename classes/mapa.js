1357833c22346e664ae9978bff9039eaa16cc343
Julio Cesar Calderon Galindo <julcescalgalty@gmail.com>
2023-10-18 16:17:33 -0500

    cambios

--- classes/mapa.js
+++ classes/mapa.js
@@ -5,6 +5,7 @@ class Mapa {
     '1':{
         id :'1',
         nombre:'Julio cesar',
+        id_vehiculo:'1',
         vehiculo:'vehiculo 1',
         lng:-74.562492,
         lat:-8.362674,
@@ -13,6 +14,7 @@ class Mapa {
     '2':{
         id :'2',
         nombre:'Juanito perez',
+        id_vehiculo:'2',
         vehiculo:'vehiculo 2',
         lng:-74.588585,
         lat:-8.383223,
@@ -21,6 +23,7 @@ class Mapa {
     '3':{
         id :'3',
         nombre:'Pedro suarez',
+        id_vehiculo:'3',
         vehiculo:'vehiculo 3',
         lng:-74.58292,
         lat:-8.411074,
--- classes/marcador.js
+++ classes/marcador.js
@@ -5,6 +5,7 @@ class Marcador{
         id ='',
         nombre='',
         vehiculo='',
+        id_vehiculo='',
         lng,
         lat,
         color=''
