56799017c1f5779d0f6f48751f02a04419b49018
Julio Cesar Calderon Galindo <juliocesar.calderong@gmail.com>
2024-02-29 12:08:16 -0500

    cambios

--- controllers/alertas-generadas.js
+++ controllers/alertas-generadas.js
@@ -43,29 +43,29 @@ const filtroAlertaGenerada = async (req = request, res = response) => {
     switch (tipo) {
       case '1':
         consulta = `
-                  select R.id, R.descripcion, R.fecha, R.hora, A.lat,A.lng, T.nombre as tipo_alerta, T.img, T.color, C.nombre, C.apellido
-                  from alerta_generada R inner join alertas A on R.id_alerta = A.id inner join tipo_alertas T on R.id_tipo_alerta= T.id inner join ciudadano C on A.ciudadano = C.id 
+                  select R.id, R.descripcion, R.fecha, R.hora, A.lat,A.lng, T.nombre as tipo_alerta, T.img, T.color, A.ciudadano, A.correo, A.celular, A.dni, T.id as id_tipo
+                  from alerta_generada R inner join alertas A on R.id_alerta = A.id inner join tipo_alertas T on R.id_tipo_alerta= T.id 
                   order by R.fecha, R.hora desc
                   `;
         break;
       case '2':
         consulta = `
-                  select R.id, R.descripcion, R.fecha, R.hora, A.lat,A.lng, T.nombre as tipo_alerta, T.img, T.color, C.nombre, C.apellido
-                  from alerta_generada R inner join alertas A on R.id_alerta = A.id inner join tipo_alertas T on R.id_tipo_alerta= T.id inner join ciudadano C on A.ciudadano = C.id 
+                  select R.id, R.descripcion, R.fecha, R.hora, A.lat,A.lng, T.nombre as tipo_alerta, T.img, T.color, A.ciudadano, A.correo, A.celular, A.dni, T.id as id_tipo
+                  from alerta_generada R inner join alertas A on R.id_alerta = A.id inner join tipo_alertas T on R.id_tipo_alerta= T.id  
                   where R.fecha>='${fechaUno}' AND R.fecha<='${fechaDos}' order by R.fecha, R.hora desc
                   `;
         break;
       case '3':
         consulta = `
-                  select R.id, R.descripcion, R.fecha, R.hora, A.lat,A.lng, T.nombre as tipo_alerta, T.img, T.color, C.nombre, C.apellido
-                  from alerta_generada R inner join alertas A on R.id_alerta = A.id inner join tipo_alertas T on R.id_tipo_alerta= T.id inner join ciudadano C on A.ciudadano = C.id 
+                  select R.id, R.descripcion, R.fecha, R.hora, A.lat,A.lng, T.nombre as tipo_alerta, T.img, T.color, A.ciudadano, A.correo, A.celular, A.dni, T.id as id_tipo
+                  from alerta_generada R inner join alertas A on R.id_alerta = A.id inner join tipo_alertas T on R.id_tipo_alerta= T.id 
                   where R.id_tipo_alerta='${tipoAlerta}  order by R.fecha, R.hora desc'
                   `;
         break;
       case '4':
         consulta = `
-                  select R.id, R.descripcion, R.fecha, R.hora, A.lat,A.lng, T.nombre as tipo_alerta, T.img, T.color, C.nombre, C.apellido
-                  from alerta_generada R inner join alertas A on R.id_alerta = A.id inner join tipo_alertas T on R.id_tipo_alerta= T.id inner join ciudadano C on A.ciudadano = C.id 
+                  select R.id, R.descripcion, R.fecha, R.hora, A.lat,A.lng, T.nombre as tipo_alerta, T.img, T.color, A.ciudadano, A.correo, A.celular, A.dni, T.id as id_tipo
+                  from alerta_generada R inner join alertas A on R.id_alerta = A.id inner join tipo_alertas T on R.id_tipo_alerta= T.id  
                   where R.fecha>='${fechaAnter}' AND R.fecha<='${fecha}' order by R.fecha, R.hora desc
                   `;
         break;
